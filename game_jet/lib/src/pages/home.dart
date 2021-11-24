09bff7a503c9ec9ee76b3730a79b00bf5cddee92
eusebiocaballero7 <eusebioo@uninorte.edu.co>
2021-11-24 01:13:15 -0500

    otro avance de home

--- game_jet/lib/src/pages/home.dart
+++ game_jet/lib/src/pages/home.dart
@@ -5,16 +5,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          AuthBackground(
-              child: Column(
-            children: [Container(
-              color: 
-            )],
-          ))
-        ],
-      ),
-    );
+        body: Stack(
+      children: [
+        AuthBackground(
+            child: Container(
+          child: Column(
+            children: [
+              Text(
+                'El juego Gartic Phone se ha vuelto muy popular gracias a los streamers. Se trata del juego de teléfono roto, donde... ver más.',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.white54),
+              ),
+            ],
+          ),
+        ))
+      ],
+    ));
   }
 }
---
+++ game_jet/lib/src/widgets/white_box.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class WhiteBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30)
+        color: 
+      ),
+    );
+  }
+}
